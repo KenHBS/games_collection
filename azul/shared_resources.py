4e0dce7f7d4f72bd28b9f29aecab8699f06f336a
KenHBS <kenschroeder@babbel.com>
2021-05-21 19:39:27 +0200

    rename to Factory and add method to take four tiles from pouch

--- azul/shared_resources.py
+++ azul/shared_resources.py
@@ -23,11 +23,17 @@ class Pouch(List[Tile]):
         super().__add__(self, add_this)
         shuffle(self)
 
+    def take_four(self, n: int = 4) -> List[Tile]:
+        """ Take last n tiles from pouch """
+        take = self[-n:]
+        del self[-n:]
+        return take
 
-class Plate(defaultdict):
+
+class Factory(defaultdict):
     """
-    This class represents the plates that are filled with 4 tiles at the start
-    of each round.
+    This class represents the factories that are filled with 4 tiles at the
+    start of each round.
     """
     def __init__(self):
         super().__init__(lambda: 0)
@@ -37,16 +43,16 @@ class Plate(defaultdict):
         return sum(self.values()) == 0
 
 
-class TheMiddle(Plate):
+class TheMiddle(Factory):
     """ This class represents 'The Middle'.
     All tiles that are not picked up when a player picks up tiles from a
-    plate are placed in the middle.
+    factory are placed in the middle.
 
-    The middle behaves like a Plate, except:
+    TheMiddle behaves like a Factory, except:
     - the middle is empty at the beginning of a round,
     - the first player to draw from the middle is penalised with 1 minus point,
     - the first player to draw from the middle is the starting player
-    in the next round.
+        in the next round.
     """
     def __init__(self):
         self.is_untouched = True

1cbe9f28ff33d494c61b803973d159e01239b9bf
KenHBS <kenschroeder@babbel.com>
2021-05-21 18:44:24 +0200

    add Plate and TheMiddle classes

--- azul/shared_resources.py
+++ azul/shared_resources.py
@@ -24,4 +24,30 @@ class Pouch(List[Tile]):
         shuffle(self)
 
 
-Plate = defaultdict
+class Plate(defaultdict):
+    """
+    This class represents the plates that are filled with 4 tiles at the start
+    of each round.
+    """
+    def __init__(self):
+        super().__init__(lambda: 0)
+
+    @property
+    def is_empty(self):
+        return sum(self.values()) == 0
+
+
+class TheMiddle(Plate):
+    """ This class represents 'The Middle'.
+    All tiles that are not picked up when a player picks up tiles from a
+    plate are placed in the middle.
+
+    The middle behaves like a Plate, except:
+    - the middle is empty at the beginning of a round,
+    - the first player to draw from the middle is penalised with 1 minus point,
+    - the first player to draw from the middle is the starting player
+    in the next round.
+    """
+    def __init__(self):
+        self.is_untouched = True
+        super().__init__()

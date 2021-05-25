8f817c41e364d3cf38fba35c6a3762da1a0d3f4d
KenHBS <kenschroeder@babbel.com>
2021-05-25 15:50:11 +0200

    add factory filling method and implement player turn cycle

--- azul/game.py
+++ azul/game.py
@@ -1,28 +1,60 @@
+
+
+# TODO: Implement TheGame class: start-player, player order, factory filling
+# TODO: Add property to indicate finished round to TheGame
+
+# TODO: Create actual game to check / test how it works
+# TODO: Figure out whether to standardise TileCounter, List(TileCounter) and
+# Factories
+
 from typing import Dict, List
+from random import choice
+from itertools import cycle
+
 from game_pieces import TheMiddle, Pouch, Factory
 from player import Player
 
 
 class TheGame:
 
-    factory_count = {
+    factory_count_mapping = {
         2: 5,
         3: 7,
         4: 9
     }
 
-    def __init__(self, player_names: List[str]):
-        self.game_will_end = False
+    def __init__(
+        self,
+        player_names: List[str],
+    ):
+        self.players = {name: Player(name) for name in player_names}
+        self.starting_player = choice(player_names)
 
-        self.players = [Player(name) for name in player_names]
         self.pouch = Pouch()
         self.the_middle = TheMiddle()
+        self.factories = self._fill_factories()
 
-        self.factory_count = TheGame.factory_count_mapping(len(player_names))
+        self.player_cycle = cycle(player_names)
 
-    def fill_factories(self) -> Dict[int, Factory]:
+    def _fill_factories(self) -> Dict[int, Factory]:
         """ Fill the factories with 4 tiles """
-        pass
+        factory_count = TheGame.factory_count_mapping[len(self.players)]
+        return {i: Factory() for i in range(factory_count)}
+
+    def _move_to_starting_player(self) -> None:
+        """ Iterate through player cycle until starting player is found """
+        for player_name in self.player_cycle:
+            if player_name == self.starting_player:
+                break
+
+    @property
+    def round_has_ended(self) -> bool:
+        """Check if all factories and the middle are empty. If so, the current
+        round is done. """
+        check1 = all(factory.is_empty() for factory in self.factories)
+        check2 = self.the_middle.is_empty()
+
+        return check1 & check2
 
     @property
     def game_will_end(self) -> bool:
